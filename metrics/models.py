1cdf8bc709a12851a542f6a5be8497e62997e542
matostr98 <matostr98@gmail.com>
2020-05-09 21:32:46 +0200

    Added registration for admin

--- metrics/models.py
+++ metrics/models.py
@@ -12,7 +12,7 @@ class PatientMetrics(models.Model):
     notes = models.CharField(max_length=1024)
 
     def __str__(self):
-        return self.patient_id + " " + self.doctor_id + " " + str(self.created) + " " + self.notes
+        return str(self.patient_id) + " " + self.doctor_id + " " + str(self.created) + " " + self.notes
 
     class Meta:
         db_table = "peselgen_metrics"
--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from attributes.models import MetricsAttributes
+from metrics.models import PatientMetrics
+from person.models import Person
+
+admin.site.register(Person)
+admin.site.register(MetricsAttributes)
+admin.site.register(PatientMetrics)
