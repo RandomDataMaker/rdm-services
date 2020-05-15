2fc483a4751f000f71d41e3c9be7a999ec68a542
Adam Przywuski <adamprzywuski98@gmail.com>
2020-05-15 23:54:01 +0200

    Adding model of data

---
+++ gelocation/gelocation_service.py
@@ -0,0 +1,33 @@
+
+from django.db import models
+import pycristoforo as pyc
+from person.models import Person
+from gelocation.models import MetricsAttributes
+import random
+import logging
+
+
+class Geolocationservice(models.Model):
+
+        def create_gelocation(self):
+            location=MetricsAttributes()
+            location.geojson = self.__generate_geolocation()
+            location.person_id=self.__generate_id()
+            logging.basicConfig(level=logging.DEBUG)
+            logging.info(
+                f"Created metrics {location.person_id} with created location {location.geojson[0]['geometry']['coordinates']}")
+            return location
+
+
+
+        def __generate_geolocation(self):
+            country = pyc.get_shape("POL")
+            points = pyc.geoloc_generation(country, 1, "POL")
+            pyc.geoloc_print(points, ',')
+
+            return points
+
+        def __generate_id(self):
+            id_list = Person.objects.filter().values_list('id', flat=True)
+            patient_id = random.choice(id_list)
+            return patient_id
